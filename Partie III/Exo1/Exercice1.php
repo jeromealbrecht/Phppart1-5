f3045481707f7c174affda009f2358cf9ff77027
December81 <megastudio@outlook.fr>
2020-12-10 14:44:26 +0100

    ok

---
+++ Partie III/Exo1/Exercice1.php
@@ -0,0 +1,14 @@
+<?php
+
+//Créer une variable et l'initialiser à 0.  
+//Tant que cette variable n'atteint pas 10, il faut :
+//- l'afficher
+//- l'incrementer
+
+$var = 0;
+
+for ($var=0; $var <=10; $var++){
+    echo $var;
+}
+
+?>
---
+++ Partie III/Exo2/Exercice2.php
@@ -0,0 +1,17 @@
+<?php
+
+// Créer deux variables. Initialiser la première à 0 et la deuxième avec un nombre compris en 1 et 100.  
+// Tant que la première variable n'est pas supérieure à 20 :
+// - multiplier la première variable avec la deuxième
+// - afficher le résultat
+// - incrementer la première variable
+
+$var1= 0;
+$var2= 5;
+
+
+while ($var1 < 20){
+
+    echo "<p>" . $var1*$var2 . "<p>";
+    echo "<p>" . $var1++ . "</p>";
+}
